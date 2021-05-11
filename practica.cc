60a530be86f2032a5944f15d2fff92b36b60e1b0
Amaro <ablest@ucm.es>
2021-05-11 19:55:14 +0200

    ADDED ejercicio 4

--- practica.cc
+++ practica.cc
@@ -17,7 +17,7 @@
     memset((void *)&hints, 0, sizeof(struct addrinfo));
 
     hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_socktype = SOCK_STREAM;
 
     int rc = getaddrinfo(argv[1],argv[2],&hints,&result);
 
@@ -34,16 +34,53 @@
         return -1;
     }
 
-    // // Asignar un nombre a un socket = -1 ha fallado en el linkeo
-    // if(bind(sd,result->ai_addr,result->ai_addrlen) == -1){
-    //     std::cerr << "[Bind] Error al asignar el nombre" << std::endl;
-    //     return -1;
-    // }
+    // Asignar un nombre a un socket = -1 ha fallado en el linkeo
+    if(bind(sd,result->ai_addr,result->ai_addrlen) == -1){
+        std::cerr << "[Bind] Error al asignar el nombre" << std::endl;
+        return -1;
+    }
 
+    int lt = listen(sd,1);
+    if(lt == -1){
+        std::cerr << "[Listen] Solo se puede conectar un cliente" << std::endl;
+        return -1;
+    }
+
+    struct sockaddr cliente;
+    socklen_t size = sizeof(struct sockaddr);
+    int ap = accept(sd ,&cliente ,&size );
+    if(ap == -1){
+        std::cerr << "[Accept] No se ha podido establecer conección" << std::endl;
+        return -1;
+    }
+
+    bool run = true;
     char mensaje [100];
-    sendto(sd ,argv[3] ,strlen(argv[3])+1 ,0 ,result->ai_addr ,result->ai_addrlen );
-    recvfrom(sd ,mensaje, 100-1 ,0 ,result->ai_addr ,&result->ai_addrlen);
-    std::cout << "Mensaje " << mensaje << std::endl;
+    //  Ip del cliente
+    char host[NI_MAXHOST];
+    //  El puerto del cliente
+    char serv[NI_MAXSERV];
+    getnameinfo(&cliente,size,host,NI_MAXHOST,serv,NI_MAXSERV,NI_NUMERICHOST);
+    std::cout << "Conexión desde " << host << " puerto " << serv << std::endl; 
+    
+    while (run)
+    {
+        ssize_t bitsDevueltos = recv(ap,mensaje,100-1,0);
+        if(bitsDevueltos == -1){
+            std::cerr << "[recvfrom]" << std::endl;
+            return -1;
+        }
+        send(ap,mensaje,bitsDevueltos,0);
+    }
+    
+    std::cout << "Conexión desde " << host << " puerto " << serv << " cerrada." << std::endl; 
+
+
+
+
+    // sendto(sd ,argv[3] ,strlen(argv[3])+1 ,0 ,result->ai_addr ,result->ai_addrlen );
+    // recvfrom(sd ,mensaje, 100-1 ,0 ,result->ai_addr ,&result->ai_addrlen);
+    // std::cout << "Mensaje " << mensaje << std::endl;
 
 
 
